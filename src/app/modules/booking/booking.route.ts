e4a8ff377c4cddac57989f7f5dd0478ca253d060
Masum Rana <masumrana446267@gmail.com>
2024-01-01 15:06:31 +0600

    set user power   according to user role

--- src/app/modules/booking/booking.route.ts
+++ src/app/modules/booking/booking.route.ts
@@ -2,18 +2,29 @@ import express from 'express';
 import validateRequest from '../../middlewares/ValidateRequest';
 import { BookingZodValidationSchema } from './bookingValidation';
 import { BookingController } from './booking.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/role';
 
 const router = express.Router();
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.CUSTOMER),
   validateRequest(BookingZodValidationSchema.BookingZodSchema),
   BookingController.makeBooking,
 );
 
 router.get('/', BookingController.getAllbookingData);
-router.patch('/:id', BookingController.updateBookingDeliveryDate);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  BookingController.updateBookingDeliveryDate,
+);
 
-router.delete('/:id', BookingController.cancelBooking);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.CUSTOMER, ENUM_USER_ROLE.SUPER_ADMIN),
+  BookingController.cancelBooking,
+);
 
 export const BookingRoutes = router;
--- src/app/modules/service/service.route.ts
+++ src/app/modules/service/service.route.ts
@@ -10,18 +10,29 @@ import express from 'express';
 import validateRequest from '../../middlewares/ValidateRequest';
 import { ServiceValidation } from './service.Validation';
 import { ServiceController } from './service.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/role';
 
 const router = express.Router();
 
 // create service by admin
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(ServiceValidation.serviceValidationSchema),
   ServiceController.createService,
 );
 
 // get Single service
-router.get('/:id', ServiceController.getSingleService);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.CUSTOMER,
+  ),
+  ServiceController.getSingleService,
+);
 
 // get All Service
 router.get('/', ServiceController.getAllService);

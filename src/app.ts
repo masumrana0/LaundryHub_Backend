633d9a7593d3217c14defe513656fba7e201bf36
Masum Rana <masumrana446267@gmail.com>
2024-02-23 09:25:58 +0600

    merge confilct resoved

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,13 @@ import router from './app/routes';
 import cookieParser from 'cookie-parser';
 const app: Application = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['https://laundryhub-client.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  }),
+);
+
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
